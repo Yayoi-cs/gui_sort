18cb2eda351414e0b068e115096f83fdfcefde3f
kokastar <103174676+starkoka@users.noreply.github.com>
2025-02-24 21:24:05 +0900

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -3,7 +3,7 @@ import random
 
 def insertionSort(data):
     n = len(data)
-    for i in range(1,n):
+    for i in range(1,n+1):
         for j in range(i-1,0,-1):
             yield ("compare", j, j-1)
             if data[j] < data[j-1]:
